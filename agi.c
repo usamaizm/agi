e53769e8e3a2a689177d5553185783308220058d
Usama Ismail <usamaizm@gmail.com>
2024-03-17 08:11:39 -0700

    Update agi.c

--- agi.c
+++ agi.c
@@ -1,1 +1,10 @@
+// agi.c
 
+agi init_agi() {
+    agi agi;
+    agi.language = init_language_module();
+    agi.vision = init_vision_module();  // Call the vision module initialization function
+    agi.reasoning = init_reasoning_module();
+    agi.kg = init_knowledge_graph();
+    return agi;
+}
