b7be08cbb4d94683a10bf1c147c82a6502833c2c
Usama Ismail <usamaizm@gmail.com>
2024-03-17 08:22:45 -0700

    Update io.h

---
+++ io.h
@@ -0,0 +1,10 @@
+// io.h
+
+#ifndef IO_H
+#define IO_H
+
+#include "module.h"  // Include any necessary headers
+
+Module init_io_module();  // Declaration of the I/O module initialization function
+
+#endif  // IO_H
