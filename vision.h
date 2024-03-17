730f54edd14bfbd8c5a9d7065d972948a9a688f9
Usama Ismail <usamaizm@gmail.com>
2024-03-17 08:09:58 -0700

    Update vision.h

---
+++ vision.h
@@ -0,0 +1,10 @@
+// vision.h
+
+#ifndef VISION_H
+#define VISION_H
+
+#include "module.h"  // Include any necessary headers
+
+Module init_vision_module();  // Declaration of the vision module initialization function
+
+#endif  // VISION_H
