e34eb8828c8d97050516e85e2b5f50382f664af6
Usama Ismail <usamaizm@gmail.com>
2024-03-17 08:32:02 -0700

    Update text.h

---
+++ text.h
@@ -0,0 +1,10 @@
+// text.h
+
+#ifndef TEXT_H
+#define TEXT_H
+
+#include "module.h"  // Include any necessary headers
+
+Module init_text_module();  // Declaration of the text module initialization function
+
+#endif  // TEXT_H
