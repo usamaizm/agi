c869177447ae78942131ebfc0b4b7fcdf3dd8440
Usama Ismail <usamaizm@gmail.com>
2024-03-18 02:12:01 -0700

    Update knowledgegraph.h

--- knowledgegraph.h
+++ knowledgegraph.h
@@ -1,15 +1,18 @@
-```c
-#ifndef KNOWLEDGE_GRAPPH_H
-#define KNOWLEDGE_GRAPPH_H
+#ifndef KNOWLEDGE_GRAPH_H
+#define KNOWLEDGE_GRAPH_H
 
 // Define the KnowledgeGraph structure
 typedef struct {
     // Define knowledge graph-specific data structures or variables if needed
 } KnowledgeGraph;
 
-KnowledgeGraph init_knowledgegraph();  // Declaration of the knowledge graph initialization function
-void store_knowledge(KnowledgeGraph kg, const char* data);  // Declaration of function to store knowledge
-void retrieve_knowledge(KnowledgeGraph kg, const char* query);  // Declaration of function to retrieve knowledge
+// Declaration of the knowledge graph initialization function
+KnowledgeGraph init_knowledgegraph();
 
-#endif  // KNOWLEDGE_GRAPPH_H
-```
+// Declaration of function to store knowledge
+void store_knowledge(KnowledgeGraph kg, const char* data);
+
+// Declaration of function to retrieve knowledge
+void retrieve_knowledge(KnowledgeGraph kg, const char* query);
+
+#endif  // KNOWLEDGE_GRAPH_H
