89286859cfe272d2bf70f7aa0b2c2d39891ed439
Usama Ismail <usamaizm@gmail.com>
2024-03-18 06:04:52 -0700

    Update speech.h

--- speech.h
+++ speech.h
@@ -6,15 +6,20 @@
 // Define the SpeechModule structure
 typedef struct {
     // Define speech module-specific data structures or variables if needed
+    // For example:
+    // const char* voice;
 } SpeechModule;
 
 // Declaration of the speech module initialization function
+// This function initializes a new instance of the speech module.
 Module init_speech_module();
 
 // Declaration of function to process speech
+// This function processes input speech using the provided speech module.
 void process_speech(Module speech_module, const char* input);
 
 // Declaration of function to free memory for the speech module
+// This function frees the memory allocated for the speech module.
 void free_speech_module(Module speech_module);
 
 #endif  // SPEECH_H
