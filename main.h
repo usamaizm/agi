dc59a450518f83f60985d4b5a2451375af6b00a4
Usama Ismail <usamaizm@gmail.com>
2024-03-18 07:06:19 -0700

    Update main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// Include necessary header files
+#include "ai.h"
+#include "text2speech.h"
+#include "speech2text.h"
+
+// Function prototypes
+void initialize_modules();
+void process_input();
+void cleanup_modules();
+
+#endif // MAIN_H
