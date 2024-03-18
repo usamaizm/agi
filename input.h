8d2ef406f4994f1012da0d91eb8877f326aa698d
Usama Ismail <usamaizm@gmail.com>
2024-03-18 01:40:02 -0700

    Update input.h

--- input.h
+++ input.h
@@ -1,17 +1,19 @@
-// input.h
-
+```c
 #ifndef INPUT_H
 #define INPUT_H
 
-#include "module.h"  // Include the header file for the generic module
-
 // Define the InputModule structure
-typedef struct InputModule {
+typedef struct {
     // Define input module-specific data structures or variables if needed
 } InputModule;
 
-Module init_input_module();  // Declaration of the input module initialization function
-void get_user_input(Module input_module);  // Declaration of function to get user input
+// Declaration of the input module initialization function
+Module init_input_module();
+
+// Declaration of function to get user input
+void get_user_input(Module input_module);
+
 // Add more input-related function prototypes as needed
 
 #endif  // INPUT_H
+```
