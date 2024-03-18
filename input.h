b8ca650d4f10ab74e283ad53e413d995f7ce2d8a
Usama Ismail <usamaizm@gmail.com>
2024-03-18 04:15:50 -0700

    Update input.h

--- input.h
+++ input.h
@@ -1,15 +1,13 @@
-#ifndef INPUT_H
-#define INPUT_H
+#ifndef INPUT_H_
+#define INPUT_H_
 
-#include "module.h"  // Include the header file for the generic module
+#include "module.h" // Include the header file for the generic module
 
-// Define the InputConfig structure
 typedef struct {
-    int input_type;  // Type of input (e.g., keyboard, mouse, etc.)
-    const char* description;  // Description of the input device
+    int input_type;        // Type of input (e.g., keyboard, mouse, etc.)
+    const char* description;   // Description of the input device
 } InputConfig;
 
-// Declaration of the input module initialization function
-Module init_input_module();
+Module init_input_module(); // Declaration of the input module initialization function
 
-#endif  // INPUT_H
+#endif // INPUT_H_
