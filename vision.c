f43ed8f7be1ca6fcb33e15ee47ed87fc8a908dde
Usama Ismail <usamaizm@gmail.com>
2024-03-18 00:59:40 -0700

    Update vision.c

--- vision.c
+++ vision.c
@@ -1,11 +1,23 @@
+```c
 // vision.c
 
 #include "vision.h"  // Include the header file for the vision module
 
+typedef struct {
+    int height;
+    int width;
+} ConfiguredVision;
+
+void config_vision(ConfiguredVision *output) {
+    output->height = 320;
+    output->width = 480;
+}
+
 Module init_vision_module() {
-    // Initialization logic for the vision module
-    // This function should return a Module object initialized with vision capabilities
     Module vision_module;
-    // Initialize vision capabilities here
+    vision_module.vision = malloc(sizeof(ConfiguredVision));
+    config_vision((ConfiguredVision *) vision_module.vision);
+
     return vision_module;
 }
+```
