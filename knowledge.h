f04acc01a016fea098cf5263447a393fb2c31fb2
Usama Ismail <usamaizm@gmail.com>
2024-03-18 03:59:12 -0700

    Update knowledge.h

---
+++ knowledge.h
@@ -0,0 +1,29 @@
+#ifndef KNOWLEDGE_H
+#define KNOWLEDGE_H
+
+#include "module.h"  // Include necessary headers
+
+// Define the KnowledgeConfig structure
+typedef struct {
+    int nodes;  // Number of nodes in the knowledge graph
+    int edges;  // Number of edges in the knowledge graph
+} KnowledgeConfig;
+
+// Define the Knowledge structure
+typedef struct {
+    const char* type;  // Type of the knowledge graph
+    int nodes;  // Number of nodes in the knowledge graph
+    int edges;  // Number of edges in the knowledge graph
+    KnowledgeConfig* config;  // Configuration for the knowledge graph
+} Knowledge;
+
+// Declaration of the knowledge initialization function
+Knowledge init_knowledge();
+
+// Declaration of function to store knowledge in the knowledge graph
+void store_knowledge(Knowledge* knowledge, const char* data);
+
+// Declaration of function to retrieve knowledge from the knowledge graph
+void retrieve_knowledge(Knowledge* knowledge, const char* query);
+
+#endif  // KNOWLEDGE_H
