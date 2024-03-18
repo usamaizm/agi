22eec95be84f9bfa8ac78da056f4304ede066f61
Usama Ismail <usamaizm@gmail.com>
2024-03-18 04:31:20 -0700

    Update io.c

--- io.c
+++ io.c
@@ -3,14 +3,14 @@
 
 #include "module.h" // Include the header file for the generic module
 
-// Define the configuration structure for I/O module
+// Define the configuration structure for the I/O module
 typedef struct {
-    int io_type;          // Type of I/O (e.g., console, file, etc.)
+    int io_type;               // Type of I/O (e.g., console, file, etc.)
     const char* description;   // Description of the I/O device
 } IOConfig;
 
 // Add a blank line for readability
-// Declaration of the initialization function for I/O module
+// Declaration of the initialization function for the I/O module
 Module init_io_module();
 
 #endif // IO_MODULE_H_
